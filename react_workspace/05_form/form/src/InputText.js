46f1ba51b146b35cb906c87e5bead72c57ea751d
JangYeHoon <jkjk128jkjk@gmail.com>
2021-03-05 09:33:18 +0900

    react form

---
+++ react_workspace/05_form/form/src/InputText.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class InputText extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value : ''};
+    }
+
+    handleSubmit = (e) => {
+        alert(this.state.value);
+        e.preventDefault();
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value : e.target.value,
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+export default InputText;
