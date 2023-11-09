c3c2c99dcb61e06696eb8b77b369d81a46637fc7
JangYeHoon <jkjk128jkjk@gmail.com>
2023-11-09 21:45:34 +0900

    Modify pylog app

--- Django_입문/pylog/blog/views.py
+++ Django_입문/pylog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from blog.models import Post
+from blog.models import Post, Comment
 
 # Create your views here.
 def post_list(request):
@@ -11,6 +11,12 @@ def post_list(request):
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        comment_content = request.POST['comment']
+        Comment.objects.create(
+            post=post,
+            content=comment_content,
+        )
     context = {
         'post': post,
     }
@@ -20,9 +26,11 @@ def post_add(request):
     if request.method == 'POST':
         title =request.POST['title']
         content = request.POST['content']
+        thumbnail = request.FILES['thumbnail']
         post = Post.objects.create(
             title=title,
             content=content,
+            thumbnail=thumbnail,
         )
         return redirect(f'/posts/{post.id}')
     else:
