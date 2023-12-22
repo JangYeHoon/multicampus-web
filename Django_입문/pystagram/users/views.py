d00d1b6cecdc6e7a7481d85501c0012be2afd0c3
JangYeHoon <jkjk128jkjk@gmail.com>
2023-12-22 13:31:24 +0900

    Update pystagram app

--- Django_입문/pystagram/users/views.py
+++ Django_입문/pystagram/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from users.forms import LoginForm, SignupForm
+from users.models import User
 
 # Create your views here.
 def login_view(request):
@@ -41,3 +42,10 @@ def signup(request):
         form = SignupForm()
     context = {'form': form}
     return render(request, 'users/signup.html', context)
+
+def profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    context = {
+        'user': user,
+    }
+    return render(request, 'users/profile.html', context)
