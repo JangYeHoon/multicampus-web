8f466ad94aecdce2b409be06eeba817831e4a5e7
JangYeHoon <jkjk128jkjk@gmail.com>
2024-04-21 21:08:19 +0900

    finish django rest board app

---
+++ Django_입문/board/posts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from users.serializers import ProflieSerializer
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    profile = ProflieSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('pk', 'profile', 'post', 'text')
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('post', 'text')
+
+class PostSerializer(serializers.ModelSerializer):
+    profile = ProflieSerializer(read_only=True)
+    comment = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('pk', 'profile', 'title', 'body', 'image', 'published_date', 'likes')
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'category', 'body', 'image')
